4556658b7ee79e68efee8d33969773081cb5bbfd
matt holmes <matthewaaronholmes@gmail.com>
2015-03-05 11:57:19 -0600

    Update ipdrop.sh

--- ipdrop.sh
+++ ipdrop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Quickly made with brevity by @bolg 
+#    Quickly made with brevity by @phzpw 
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
